5a51598514b216f5f6863ca97808c4e14c7bb27b
amidg <amidg4x4dima@gmail.com>
2022-03-30 22:15:21 -0400

    ignoring .txt file

--- anti-lightshot.go
+++ anti-lightshot.go
@@ -14,9 +14,13 @@ import (
 import (
 	"errors"
 	"flag"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"math/rand"
 	"net/http"
+	"runtime"
+	"strings"
 
 	"github.com/schollz/progressbar/v3"
 )
@@ -30,10 +34,35 @@ var (
 	downloadedImageBuffer  []string // dynamic slice of image IDs
 	newImageID             string
 	previousImageID        string
+	imgurImageID           = "https://i.imgur.com/y5Onqkp.png"
 )
 
 /////////////////////////////////////////////////////////////
 //FUNCTIONS:
+func eliminateNewLineCrap(text string) string {
+	os := runtime.GOOS
+	if os == "windows" {
+		return (strings.Replace(text, "\r\n", "", -1))
+	} else {
+		return (strings.Replace(text, "\n", "", -1))
+	}
+}
+
+func trimStringBetweenTwo(input string, startS string, endS string) (result string, found bool) {
+	s := strings.Index(input, startS)
+	if s == -1 {
+		return result, false
+	}
+
+	newS := input[s+len(startS):]
+	e := strings.Index(newS, endS)
+	if e == -1 {
+		return result, false
+	}
+	result = newS[:e]
+	return result, true
+}
+
 func generateRandomImageID() string {
 	var newImageId string
 	for i := 0; i < 6; i++ {
@@ -43,10 +72,12 @@ func generateRandomImageID() string {
 	return newImageId
 }
 
-func downloadImageByName(imageID string) error {
+func downloadImageByURL(URL string) error {
 	//Get the response bytes from the url
-	var fullImageURL = "https://prnt.sc/" + imageID
-	response, err := http.Get(fullImageURL)
+	URL = eliminateNewLineCrap(URL)
+	imageIDFromURL, _ := trimStringBetweenTwo(URL, "https://i.imgur.com/", ".png")
+
+	response, err := http.Get(URL)
 	if err != nil {
 		return err
 	}
@@ -56,7 +87,7 @@ func downloadImageByName(imageID string) error {
 		return errors.New("Received non 200 response code")
 	}
 	//Create a empty file
-	file, err := os.Create(pathToDownloadedImages + imageID)
+	file, err := os.Create(pathToDownloadedImages + imageIDFromURL + ".png")
 	if err != nil {
 		return err
 	}
@@ -71,10 +102,74 @@ func downloadImageByName(imageID string) error {
 	return nil
 }
 
+func getPageHTML(url string) string {
+	// return doc.FullText()
+	// resp, err := http.Get(url)
+	// fmt.Println(resp.ContentLength)
+	// resp.Header.Add("Accept", "application/xml")
+	// resp.Header.Add("Content-Type", "application/xml; charset=utf-8")
+	// if err != nil {
+	// 	// handle error
+	// }
+	// defer resp.Body.Close()
+
+	// htmlbody, err := ioutil.ReadAll(resp.Body)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	return "htmlbody"
+}
+
+func downloadImageByLightshotID(imageID string) error {
+	// 0. create image URL
+	var imageLightshotURL = "https://prnt.sc/" + imageID
+
+	// 1. parse the HTML page
+	res, err := http.Get(imageLightshotURL)
+	if err != nil {
+		// handle error
+	}
+	defer res.Body.Close()
+
+	htmlbody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s\n", htmlbody)
+
+	// //Get the response bytes from the url
+	// var fullImageURL = "https://prnt.sc/" + imageID
+	// response, err := http.Get(fullImageURL)
+	// if err != nil {
+	// 	return err
+	// }
+	// defer response.Body.Close()
+
+	// if response.StatusCode != 200 {
+	// 	return errors.New("Received non 200 response code")
+	// }
+	// //Create a empty file
+	// file, err := os.Create(pathToDownloadedImages + imageID + ".png")
+	// if err != nil {
+	// 	return err
+	// }
+	// defer file.Close()
+
+	// //Write the bytes to the fiel
+	// _, err = io.Copy(file, response.Body)
+	// if err != nil {
+	// 	return err
+	// }
+
+	return nil
+}
+
 func recordDownloadedImageID(imageID string) {
 	logfile, openerr := os.OpenFile(pathToLogFile, os.O_APPEND|os.O_WRONLY, 0644)
 
-	_, logerr := logfile.WriteString(imageID + "\n")
+	_, logerr := logfile.WriteString("https://prnt.sc/" + imageID + "\n")
 
 	if openerr != nil || logerr != nil {
 		println("file error")
@@ -94,21 +189,38 @@ func recordDownloadedImageID(imageID string) {
 
 func main() {
 	howManyImages := flag.Int("howmanyimages", 0, "specify how many images to download")
+	specifyImageURL := flag.String("imageurl", "", "you can specify image url here, e.g. --imageurl https://i.imgur.com/y5Onqkp.png")
+	readPageHTML := flag.String("readhtml", "", "read page html of the specified address, e.g. --readhtml https://i.imgur.com/y5Onqkp.png")
+	//slowMode := flag.Bool("slowmode", false, "use slowmode to pre-define image id before downloading them")
 	flag.Parse()
 
-	downloadingProgressBar := progressbar.DefaultBytes(
-		(int64(*howManyImages)),
-		"Downloading",
-	)
-	for i := 0; i < (*howManyImages); i++ {
-		newImageID = generateRandomImageID()
-		if newImageID != previousImageID {
-			recordDownloadedImageID(newImageID)
-			downloadImageByName(newImageID)
-			previousImageID = newImageID
-			downloadingProgressBar.Add(1)
-		} else {
-			i = i - 1
+	if *readPageHTML != "" {
+		fmt.Printf("%s\n", string(getPageHTML(*readPageHTML)))
+		os.Exit(3)
+	} else if *specifyImageURL != "" {
+		fmt.Print("you provided image url: ")
+		fmt.Println(*specifyImageURL)
+		downloadImageByURL(*specifyImageURL)
+	} else if *specifyImageURL == "" && *howManyImages != 0 {
+		fmt.Print("you specified images: ")
+		fmt.Println(*howManyImages)
+		downloadingProgressBar := progressbar.DefaultBytes(
+			(int64(*howManyImages)),
+			"Downloading",
+		)
+		for i := 0; i < (*howManyImages); i++ {
+			newImageID = generateRandomImageID()
+			if newImageID != previousImageID && newImageID != "" {
+				recordDownloadedImageID(newImageID)
+				downloadImageByLightshotID(newImageID)
+				previousImageID = newImageID
+				downloadingProgressBar.Add(1)
+			} else {
+				i = i - 1
+			}
 		}
+	} else {
+		fmt.Println("no command provided")
+		os.Exit(3)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,11 +3,14 @@ module WolfWisdom
 go 1.15
 
 require (
+	github.com/anaskhan96/soup v1.2.5 // indirect
 	github.com/fogleman/gg v1.3.0
 	github.com/ghiac/bimg v1.0.19 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/hahnicity/go-wget v0.0.0-20130821055227-3e7ca2f20ef2 // indirect
 	github.com/schollz/progressbar/v3 v3.8.6 // indirect
 	golang.org/x/crypto v0.0.0-20220321153916-2c7772ba3064 // indirect
 	golang.org/x/image v0.0.0-20200927104501-e162460cd6b5 // indirect
+	golang.org/x/net v0.0.0-20220325170049-de3da57026de // indirect
 	golang.org/x/sys v0.0.0-20220330033206-e17cdc41300f // indirect
 )
