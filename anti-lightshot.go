ba52817d74df2c0ac6509f1aa28cfd3a0d0e9b99
amidg <amidg4x4dima@gmail.com>
2022-03-31 21:52:01 -0400

    downloading works

--- anti-lightshot.go
+++ anti-lightshot.go
@@ -32,7 +32,7 @@ var (
 	pathToDownloadedImages = "./DownloadedContent/"
 	pathToLogFile          = "./downloadlog.txt"
 	imageIDcharacters      = [36]string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
-	downloadedImageBuffer  []string // dynamic slice of image IDs
+	predefinedImageBuffer  []string // dynamic slice of image IDs
 	newImageID             string
 	previousImageID        string
 	imgurImageID           = "https://i.imgur.com/y5Onqkp.png"
@@ -125,10 +125,9 @@ func getPageHTML(url string) (content string) {
 	return filecontent
 }
 
-func downloadImageByURL(URL string) error {
+func downloadImageByURL(URL string, lightshotID string) error {
 	//Get the response bytes from the url
 	URL = eliminateNewLineCrap(URL)
-	imageIDFromURL, _ := trimStringBetweenTwo(URL, "https://i.imgur.com/", ".png")
 
 	response, err := http.Get(URL)
 	if err != nil {
@@ -140,7 +139,7 @@ func downloadImageByURL(URL string) error {
 		return errors.New("Received non 200 response code")
 	}
 	//Create a empty file
-	file, err := os.Create(pathToDownloadedImages + imageIDFromURL + ".png")
+	file, err := os.Create(pathToDownloadedImages + lightshotID + ".png")
 	if err != nil {
 		return err
 	}
@@ -169,52 +168,66 @@ func GetStringInBetweenTwoString(str string, startS string, endS string) (result
 	return result, true
 }
 
-func getActualImageLink(parsedhtml string) string {
+func getActualImageLink(parsedhtml string) (string, bool) {
 	// 1. get string between those values
-	imageURL, _ := trimStringBetweenTwo(parsedhtml, "https://i.imgur.com/", " ")
+	imageURL, isFound := trimStringBetweenTwo(parsedhtml, "https://i.imgur.com/", " ")
 
-	return "https://i.imgur.com/" + imageURL[:len(imageURL)-3]
+	if len(imageURL) < 6 || !isFound {
+		imageURL = ""
+	} else if isFound {
+		imageURL = ("https://i.imgur.com/" + imageURL[:len(imageURL)-3])
+	}
+
+	return imageURL, isFound
 }
 
-func downloadImageByLightshotID(imageID string) error {
+func downloadImageByLightshotID(imageID string) (bool, error) {
 	// 0. create image URL
 	imageID = eliminateNewLineCrap(imageID)
 	var imageLightshotURL = "https://prnt.sc/" + imageID
+	var error error
 
-	// 1. get file with certain ID
-	var isLightshotHacked = false
-	downloadLightshotCommand := exec.Command("wget", imageLightshotURL)
-	_, err := downloadLightshotCommand.Output()
-	if err != nil {
-		fmt.Println("error downloading file")
-	} else if err == nil {
-		for {
-			_, err := os.Stat(imageID)
-			if !os.IsExist(err) {
-				break
-			}
-		}
-
-		isLightshotHacked = true
-	}
-
-	// 2. read their crap
-	if isLightshotHacked {
-		downloadImageByURL(getActualImageLink(imageID))
-		e := os.Remove(imageID)
-		if e != nil {
-			fmt.Println("file does not exit")
+	actualImageURL, isFound := getActualImageLink(getPageHTML(eliminateNewLineCrap(imageLightshotURL)))
+	if isFound {
+		error = downloadImageByURL(actualImageURL, imageID)
+		if error != nil {
+			fmt.Println("error downloading image")
 			os.Exit(3)
 		}
+	} else {
+		isFound = false
 	}
 
-	return nil
+	return isFound, error
+}
+
+func predefineImageBuffer(imagecount int) {
+	_, logSlice := readfile(pathToLogFile)
+	currentlog := make([]string, len(logSlice))
+	copy(currentlog, logSlice)
+	for i := 0; i < imagecount; i++ {
+		newImageID = generateRandomImageID()
+		logLength := len(currentlog)
+		switch logLength {
+		case 0:
+			predefinedImageBuffer = append(predefinedImageBuffer, newImageID)
+			previousImageID = newImageID
+		default:
+			if newImageID != previousImageID && newImageID != "" && (newImageID != currentlog[i] || currentlog[i] == "") {
+				predefinedImageBuffer = append(predefinedImageBuffer, newImageID)
+				previousImageID = newImageID
+			} else {
+				i = i - 1
+			}
+		}
+	}
 }
 
 func recordDownloadedImageID(imageID string) {
 	logfile, openerr := os.OpenFile(pathToLogFile, os.O_APPEND|os.O_WRONLY, 0644)
 
-	_, logerr := logfile.WriteString("https://prnt.sc/" + imageID + "\n")
+	//_, logerr := logfile.WriteString("https://prnt.sc/" + imageID + "\n")
+	_, logerr := logfile.WriteString(imageID + "\n")
 
 	if openerr != nil || logerr != nil {
 		println("file error")
@@ -248,25 +261,31 @@ func main() {
 	} else if *specifyImageURL != "" {
 		fmt.Print("you provided image url: ")
 		fmt.Println(*specifyImageURL)
-		downloadImageByURL(*specifyImageURL)
+		downloadImageByURL(*specifyImageURL, "imageIDshouldBeHere")
 	} else if *specifyImageURL == "" && *howManyImages != 0 {
+		var totalImagesDownloaded int
 		fmt.Print("you specified images: ")
 		fmt.Println(*howManyImages)
+		predefineImageBuffer(*howManyImages)
 		downloadingProgressBar := progressbar.DefaultBytes(
 			(int64(*howManyImages)),
 			"Downloading",
 		)
 		for i := 0; i < (*howManyImages); i++ {
-			newImageID = generateRandomImageID()
-			if newImageID != previousImageID && newImageID != "" {
-				recordDownloadedImageID(newImageID)
-				downloadImageByLightshotID(newImageID)
-				previousImageID = newImageID
-				downloadingProgressBar.Add(1)
-			} else {
-				i = i - 1
+			recordDownloadedImageID(predefinedImageBuffer[i])
+			isFound, _ := downloadImageByLightshotID(predefinedImageBuffer[i])
+			downloadingProgressBar.Add(1)
+
+			if isFound {
+				totalImagesDownloaded = totalImagesDownloaded + 1
+				isFound = false
 			}
 		}
+
+		fmt.Print("Success rate: ")
+		fmt.Print(100 * float32(totalImagesDownloaded) / float32(*howManyImages))
+		fmt.Println(" %")
+
 	} else {
 		fmt.Println("no command provided")
 		os.Exit(3)
